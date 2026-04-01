c8f3d6f6cdcc7b53eb55611f335ab2503d1ca749
D KIRITHEKA <kirithekad@gmail.com>
2026-04-01 23:38:07 +0530

    Create boundedarr.sv

---
+++ Array/boundedarr.sv
@@ -0,0 +1,18 @@
+module tb;
+
+  int arr[5];   
+
+  initial begin
+    
+    arr[0] = 10;
+    arr[1] = 20;
+    arr[2] = 30;
+    arr[3] = 40;
+    arr[4] = 50;
+
+    foreach(arr[i])
+      $display("array value = %0d", arr[i]);
+
+  end
+
+endmodule
